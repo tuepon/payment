44ac55020c22c8127788f2e98d920bbd76c2ac71
Tomonori Ueno <t.uepon@gmail.com>
2019-01-09 23:37:22 +0900

    navbar is weired

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,19 +1,15 @@
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="ナビゲーションの切替">
+          <span class="navbar-toggler-icon"></span>
+        </button>
           <a class="navbar-brand" href="/">Payment</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="{{Request::is('/') ? 'active' : ''}}"><a href="/">Home</a></li>
-            <li class="{{Request::is('payment/create') ? 'active' : ''}}"><a href="payment/create">Create Payment</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
+          <div id="navbar" class="collapse navbar-collapse">
+              <!--<div id="navbar" class="collapse navbar-collapse"> -->
+              <ul class="navbar-nav">
+                <li class="{{Request::is('/') ? 'active' : ''}}"><a href="/">Home</a></li>
+                <li class="{{Request::is('payment/create') ? 'active' : ''}}"><a href="payment/create">Create Payment</a></li>
+              </ul>
+          </div><!--/.nav-collapse -->
       </div>
 </nav>
--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
   <h1>Payment</h1>
-  @if(count($payments) > 0)
-    @foreach($payments as $payment)
-      <div class="well">
-        <h3>{{$payment->id}}</h3><span class="label label-danger">{{$payment->payment}}円</span>
-      </div>
-    @endforeach
-  @endif
+    @if(count($payments) > 0)
+      @foreach($payments as $payment)
+        <div class="well">
+          <h3>{{$payment->id}} <span class="badge badge-primary">{{$payment->payment}}円</span></h3>
+        </div>
+      @endforeach
+    @endif
 @endsection
