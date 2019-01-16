7a7aacd1b18f2c4ff90af85ed9b0c67954a3ecd0
Tomonori Ueno <t.uepon@gmail.com>
2019-01-17 05:12:48 +0900

    ID link bug fixed

--- resources/views/payments/show.blade.php
+++ resources/views/payments/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <a class="btn btn-default" href="/">戻る</a>
-  <h1><a href="payment/{{$payment->id}}">{{$payment->id}}</a></h1>
+  <h1><a href="/payment/{{$payment->id}}">{{$payment->id}}</a></h1>
   <h2>{{$payment->payment}}円</h2>
   <h3>月度:<div class="badge badge-success">{{$payment->due}}</div></h3>
   <div>登録日:{{$payment->created_at->format('Y/m/d')}}</div>
