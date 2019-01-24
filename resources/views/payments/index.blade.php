2cc7057152e0a485f54364e7a602dad1833f40aa
Tomonori Ueno <t.uepon@gmail.com>
2019-01-25 07:11:48 +0900

    Modified View.

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -2,12 +2,25 @@
 
 @section('content')
   <h1>入金一覧</h1>
+  <p>子どもの学費積立記録です。</p>
     @if(count($payments) > 0)
       @foreach($payments as $payment)
-        <div class="well">
-          <h2><span class="badge badge-success">{{$payment->due}}</span>
-          {{$payment->payment}}円&nbsp;&nbsp;ID:<a href="payment/{{$payment->id}}">{{$payment->id}}</a></h2>
-          
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>月度</th>
+            <th>金額</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{$payment->due}}</td>
+            <td>{{$payment->payment}}円</td>
+            <td><a href="payment/{{$payment->id}}">{{$payment->id}}</a></td>
+          </tr>
+        </tbody>
+      </table>
       @endforeach
     @endif
 @endsection
--- storage/137bb96e77d85a04a3ab531571677076531c0d9d.php
+++ storage/137bb96e77d85a04a3ab531571677076531c0d9d.php
@@ -1,11 +1,24 @@
 <?php $__env->startSection('content'); ?>
   <h1>入金一覧</h1>
+  <p>子どもの学費積立記録です。</p>
     <?php if(count($payments) > 0): ?>
       <?php $__currentLoopData = $payments; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $payment): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <div class="well">
-          <h2><span class="badge badge-success"><?php echo e($payment->due); ?></span>
-          <?php echo e($payment->payment); ?>円&nbsp;&nbsp;ID:<a href="payment/<?php echo e($payment->id); ?>"><?php echo e($payment->id); ?></a></h2>
-          
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>月度</th>
+            <th>金額</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><?php echo e($payment->due); ?></td>
+            <td><?php echo e($payment->payment); ?>円</td>
+            <td><a href="payment/<?php echo e($payment->id); ?>"><?php echo e($payment->id); ?></a></td>
+          </tr>
+        </tbody>
+      </table>
       <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     <?php endif; ?>
 <?php $__env->stopSection(); ?>
